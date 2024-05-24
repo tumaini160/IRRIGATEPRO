2102ecec879643064eb2cbed196bf1fc44f91672
Tumaini E Meela <tumainimeela160@gmailcom>
2024-05-25 01:02:36 +0300

    sensor data

--- IRRIGATEPRO/firebase.py
+++ IRRIGATEPRO/firebase.py
@@ -1,7 +1,7 @@
 import firebase_admin
 from firebase_admin import credentials, db
 
-cred = credentials.Certificate('path/to/irrigatepro-524d5-firebase-adminsdk-ndsw9-57e6cc54b5.json')
+cred = credentials.Certificate('irrigatepro-524d5-firebase-adminsdk-ndsw9-57e6cc54b5.json')
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://irrigatepro-524d5.firebaseio.com'
 })
